5066a393d5b14ab7a766b8223245aad12dd6a534
Curtis Liu <ccc7711@gmail.com>
2020-11-16 13:25:25 -0800

    Fixed base sample to match other sample

--- src/App.java
+++ src/App.java
@@ -6,11 +6,14 @@ public class App {
         SudokuGame game = new SudokuGame();
         
         setupGame1(game);
+        SudokuGame game2 = setupGame2();
         game.displayGame();
+        game2.displayGame();
 
-        while (game.getCompleted() == false) {
-            // do nothing
-        }
+        // while (game.getCompleted() == false) {
+        //     // do nothing
+            
+        // }
 
         
     }
@@ -21,9 +24,9 @@ public class App {
                 {0,0,0,2,6,0,7,0,1}, // Row 0
                 {6,8,0,0,7,0,0,9,0}, // Row 1
                 {1,9,0,0,0,4,5,0,0}, // Row 2
-                {8,2,0,0,0,1,4,0,0}, // Row 3
+                {8,2,0,1,0,0,0,4,0}, // Row 3
                 {0,0,4,6,0,2,9,0,0}, // Row 4
-                {0,5,0,0,0,3,0,2,1}, // Row 5
+                {0,5,0,0,0,3,0,2,8}, // Row 5
                 {0,0,9,3,0,0,0,7,4}, // Row 6
                 {0,4,0,0,0,5,0,3,6}, // Row 7
                 {7,0,3,0,1,8,0,0,0}, // Row 8
